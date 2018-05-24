a6a8d162d72ef63fb8dc9d49bcc4cbd142f4093b
David Stewart <sirdavethebrave@googlemail.com>
2018-05-24 14:36:33 +0100

    house template wip

--- themes/chavasse-ferme/index.php
+++ themes/chavasse-ferme/index.php
@@ -1,24 +1,24 @@
 <?php
-
-/* Main Template File */
-
-get_header();
-
+    /* Main Template File */
+    get_header();
 ?>
 
 <div class="container-fluid">
     <h1>Chavasse Farm</h1>
-
     <div class="main_content">
-
+        <?php
+            // start the loop
+            if(have_posts()) :
+                while(have_posts()) :
+                    the_post();
+                    the_content();
+                endwhile;
+            endif;
+        ?>
     </div>
 </div>
 
 
-
-
-    <?php
-
-get_footer();
-
+<?php
+    get_footer();
 ?>
